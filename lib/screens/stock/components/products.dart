c45b362f8b59de84d3604d23a002ce0be99d78cc
GokupcWork <gokupc21@gmail.com>
2022-06-08 15:43:39 +0530

    error solved | problems [Text(data.nameEN), versions]

--- lib/screens/stock/components/products.dart
+++ lib/screens/stock/components/products.dart
@@ -3,6 +3,8 @@ import '../../../constants.dart';
 import 'package:omnath_agritech_web/responsive.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../stock_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:paginate_firestore/paginate_firestore.dart';
 
 class Products extends StatefulWidget {
   @override
@@ -12,6 +14,7 @@ class Products extends StatefulWidget {
 class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
       padding: const EdgeInsets.all(defaultPadding),
       decoration: const BoxDecoration(
@@ -65,6 +68,28 @@ class _ProductsState extends State<Products> {
         const SizedBox(
           height: 20,
         ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Container(
+            height: size.height * 0.85,
+            child: PaginateFirestore(
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, documentSnapshots, index) {
+                final data = documentSnapshots[index].data() as Map?;
+                return ProductCard(
+                  data: data,
+                );
+              },
+              query: FirebaseFirestore.instance.collection('Product').orderBy('nameEN'),
+              itemBuilderType: PaginateBuilderType.gridView,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: (size.width * 0.5 / size.height * 0.5),
+              ),
+              isLive: true,
+            ),
+          ),
+        ),
       ]),
     );
   }
@@ -96,6 +121,141 @@ class SearchField extends StatelessWidget {
               borderRadius: const BorderRadius.all(Radius.circular(10)),
             ),
             child: SvgPicture.asset("assets/icons/Search.svg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({Key? key, this.data}) : super(key: key);
+  final data;
+  firstImage() {
+    List images = data['images'];
+    return images.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: size.width * 0.02,
+        vertical: size.height * 0.01,
+      ),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(6)),
+              color: Colors.grey.shade100),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: size.width * 0.02,
+              vertical: size.height * 0.01,
+            ),
+            child: Row(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Center(
+                    //   child: OptimizedCacheImage(
+                    //     imageUrl: "${firstImage()}",
+
+                    //     errorWidget: (context, url, error) => Icon(Icons.error),
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   height: size.height * 0.03,
+                    // ),
+                    Text(data['nameEN']),
+                    // Container(
+                    //   height: size.height * 0.028,
+                    //   width: size.width * 0.08,
+                    //   decoration: BoxDecoration(
+                    //       borderRadius: BorderRadius.all(Radius.circular(4)),
+                    //       color: Colors.blue.shade900),
+                    //   child: AutoText(
+                    //     height: size.height * 0.01,
+                    //     width: size.width * 0.06,
+                    //     text: '-10%',
+                    //     style: TextStyle(
+                    //       fontSize: 18,
+                    //       color: Colors.white.withOpacity(0.5),
+                    //       fontWeight: FontWeight.bold,
+                    //     ),
+                    //     maxline: 1,
+                    //     showcolor: Colors.transparent,
+                    //     centered: true,
+                    //   ),
+                    // ),
+                    // AutoText(
+                    //   height: size.height * 0.03,
+                    //   width: size.width * 0.28,
+                    //   text: 'Fertilizer, 25kg',
+                    //   style: TextStyle(
+                    //     fontSize: 18,
+                    //     color: Colors.black.withOpacity(0.5),
+                    //     fontWeight: FontWeight.w600,
+                    //   ),
+                    //   maxline: 1,
+                    //   showcolor: Colors.transparent,
+                    //   centered: false,
+                    // ),
+                    // AutoText(
+                    //   height: size.height * 0.04,
+                    //   width: size.width * 0.28,
+                    //   text: '₹2000',
+                    //   style: TextStyle(
+                    //     fontSize: 18,
+                    //     color: Colors.blue.shade800,
+                    //     fontWeight: FontWeight.w600,
+                    //   ),
+                    //   maxline: 1,
+                    //   showcolor: Colors.transparent,
+                    //   centered: false,
+                    // ),
+                    // Align(
+                    //   alignment: Alignment.bottomCenter,
+                    //   child: Button(
+                    //     height: size.height * 0.04,
+                    //     width: size.width * 0.5,
+                    //     child: AutoText(
+                    //       height: size.height * 0.03,
+                    //       width: size.width * 0.28,
+                    //       text: '${provider.addtocart}',
+                    //       style: TextStyle(
+                    //         fontSize: 18,
+                    //         color: Colors.white.withOpacity(0.9),
+                    //         fontWeight: FontWeight.w600,
+                    //       ),
+                    //       maxline: 1,
+                    //       showcolor: Colors.transparent,
+                    //       centered: true,
+                    //     ),
+                    //     outline: false,
+                    //     onpressed: () {
+                    //       Navigation().basicNavigation(
+                    //         Product(
+                    //           data: data,
+                    //         ),
+                    //         context,
+                    //       );
+                    //     },
+                    //   ),
+                    // ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.network(firstImage()),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
